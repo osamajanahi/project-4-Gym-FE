6c36f03c229469ca674e636066c4178ab9b0352f
osamajanahi <osamaj12@gmail.com>
2024-01-15 19:44:51 +0300

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> adsdsdsnd save to reload.
+          Edit <code>src/App.js</code> adsdsdsnd save to relosdsdsad.
         </p>
         <a
           className="App-link"
