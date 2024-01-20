5fc7fb56e7259bfe347b502c84cc670767fa4497
osamajanahi <osamaj12@gmail.com>
2024-01-20 19:03:28 +0300

    DOne with class

--- src/components/class/ClassDetail.jsx
+++ src/components/class/ClassDetail.jsx
@@ -1,9 +1,10 @@
 import { React, useState, useEffect } from 'react';
 import Axios from 'axios';
 import { useParams, useNavigate } from "react-router-dom";
-export default function ClassDetail() {
+export default function ClassDetail(props) {
 
     const [classes, setClasses] = useState({});
+    const navigate = useNavigate();
     const { id } = useParams();
     useEffect(() =>{
         loadClasses(id)
@@ -19,6 +20,14 @@ export default function ClassDetail() {
         })
     }
 
+    const addUser = (ids) =>{
+        console.log(ids)
+        Axios.get(`/class/enroll?class=${ids.class}&user=${ids.user}`)
+        .then(() =>{
+            navigate('/class')
+        })
+    }
+
     return (
         <div>
             <h1>ClassDetail</h1>
@@ -28,8 +37,10 @@ export default function ClassDetail() {
                 <h3>Description: {classes.description}</h3>
                 <h3>Images:</h3>
                 {classes.image?.map((image, index) => (
-                    <img key={index} src={image} alt='class image' />
+                    <img key={index} src={image} alt='class image' height={100} width={100}/>
                 ))}
+                <br />
+                <button onClick={() => addUser({"class": classes._id, "user": props.userId})}>Enroll</button>
         </div>
     )
 }
